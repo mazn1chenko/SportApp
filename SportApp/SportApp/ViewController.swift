c84aa65dddff1a0e0fdee6a683ae3ea858e717c8
mazn1chenko <maznichenko.144@gmail.com>
2023-11-08 20:48:39 +0200

    Start
    Create new repo + app folder

---
+++ SportApp/SportApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportApp
+//
+//  Created by m223 on 08.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
